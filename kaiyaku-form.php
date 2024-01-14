49ff3af607415238ead1a164bc60bcb93de532a5
AndreyMpr <andreympr@gmail.com>
2024-01-14 22:35:47 +0900

    Create a new php file for the kaiyaku form, that's going to be based on our own kaiyaku process.

---
+++ kaiyaku-form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>解約受付フォーム</title>
+</head>
+<body>
+
+<h1>解約受付フォーム</h1>
+    
+</body>
+</html>
