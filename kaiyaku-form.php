cb29b6d30141d93dda4a4f6994e98659695356fe
AndreyMpr <andreympr@gmail.com>
2024-01-19 22:12:10 +0900

    Add an inner tag to the stylesheet, to narrow the page to 1070px.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -9,17 +9,17 @@
 <body>
 
 <h1>解約受付フォーム</h1>
-<div>
+<div class="inner">
     <p>解約をご希望の際は、当フォームより解約申請をお願い致します。</p>
     <p>転居先のご住所等、未定の項目は「未定」とご記入ください。</p>
     <p>解約を受付後、ご入力いただいたお電話番号へ弊社より、ご連絡をさせていただきます。</p>
 </div>
-<div>
+<div class="inner">
     <p>最短解約日：2ヶ月　or　1ヶ月　（契約書をご確認ください）</p>
     <p>例）7/21受付→9/21（2か月前）or　8/21（1ヶ月前）</p>
 </div>
 
-<section>
+<section class="inner">
     <form action="kaiyaku-form-handler.php" method="get">
 
         <div>
