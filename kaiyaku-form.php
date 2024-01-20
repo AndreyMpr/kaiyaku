220baae73d4c082c4b3f9586a5e5afaa685fda6b
AndreyMpr <andreympr@gmail.com>
2024-01-20 23:36:36 +0900

    Add class to all labels with input field.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -23,37 +23,37 @@
     <form action="kaiyaku-form-handler.php" method="get">
 
         <div class="question">
-            <label for="contractor-name">契約者氏名</label>
-            <input type="text" id="contractor-name" name="contractor-name" placeholder="山田太郎">
+            <label class="wide-label" for="contractor-name">契約者氏名</label>
+            <input class="wide-input" type="text" id="contractor-name" name="contractor-name" placeholder="山田太郎">
         </div>
 
         <div class="question">
-            <label for="property-name">物件名</label>
+            <label class="wide-label" for="property-name">物件名</label>
             <input type="text" id="property-name" name="property-name" placeholder="桜木マンション">
         </div>
         
         <div class="question">
-            <label for="room-number">号室</label>
+            <label class="wide-label" for="room-number">号室</label>
             <input type="text" id="room-number" name="room-number" placeholder="101">
         </div>
 
         <div class="question">
-            <label for="parking-number">駐車場・№</label>
+            <label class="wide-label" for="parking-number">駐車場・№</label>
             <input type="text" id="parking-number" name="parking-number" placeholder="01">
         </div class="question">
 
         <div class="question">
-            <label for="phone-number">電話番号</label>
+            <label class="wide-label" for="phone-number">電話番号</label>
             <input type="tel" id="phone-number" name="phone-number" placeholder="048-729-8661">
         </div>
 
         <div class="question">
-            <label for="move-out-date">退去予定日（立会日）</label>
+            <label class="wide-label" for="move-out-date">退去予定日（立会日）</label>
             <input type="date" id="move-out-date" name="move-out-date" >
         </div>
 
         <div class="question">
-            <label for="move-out-time">立会希望時間</label>
+            <label class="wide-label" for="move-out-time">立会希望時間</label>
             <input type="time" id="move-out-time" name="move-out-time">
         </div>
 
@@ -95,22 +95,22 @@
         </div>
 
         <div class="question">
-            <label for="address">転居先住所</label>
+            <label class="wide-label" for="address">転居先住所</label>
             <input type="text" id="address" name="address">
         </div>
 
         <div class="question">
-            <label for="bank-account">敷金等返金先口座</label>
+            <label class="wide-label" for="bank-account">敷金等返金先口座</label>
             <input type="text" id="bank-account" name="bank-account" placeholder="埼玉りそな銀行　大宮西支店　普通　1474654">
         </div>
         
         <div class="question">
-            <label for="bank-name">口座名義</label>
+            <label class="wide-label" for="bank-name">口座名義</label>
             <input type="text" id="bank-name" name="bank-name" placeholder="カ）サイタママルヤマコウムショ">
         </div>
 
         <div class="question">
-            <label for="comment">お住まいいただいていた期間に何か不具合がございましたか。</label>
+            <label class="wide-label" for="comment">お住まいいただいていた期間に何か不具合がございましたか。</label>
             <textarea id="comment" name="comment">
                 
             </textarea>
@@ -138,17 +138,17 @@
         </div>
 
         <div class="question">
-            <label for="explanation">上記設問にてやや不満足、不満足とお答えされた方はその理由をお聞かせください。</label>
+            <label class="wide-label" for="explanation">上記設問にてやや不満足、不満足とお答えされた方はその理由をお聞かせください。</label>
             <input type="text" id="explanation" name="explanation">
         </div>
 
         <div class="question">
-            <label for="wishes">希望する設備やサービスは何かございましたか。</label>
+            <label class="wide-label" for="wishes">希望する設備やサービスは何かございましたか。</label>
             <input type="text" id="wishes" name="wishes">
         </div>
 
         <div class="question">
-            <label for="other">その他ご質問等がございましたら、ご自由にご記入ください。</label>
+            <label class="wide-label" for="other">その他ご質問等がございましたら、ご自由にご記入ください。</label>
             <input type="text" id="other" name="other">
         </div>
 
