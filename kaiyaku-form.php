9fda3566813018dc6dfbe9d1a9309c684995704c
AndreyMpr <andreympr@gmail.com>
2024-02-01 23:46:13 +0900

    Make the number of question 8 appear on the same line as the title of the fieldset.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -81,7 +81,6 @@
 
         <div class="question">
             <fieldset>
-                <p class="number">8.</p>
                 <legend><p class="number">8.</p>退去理由</legend>
                 <p class="comment">※差し支えなければ、転居の理由をお聞かせください。</p>
 
