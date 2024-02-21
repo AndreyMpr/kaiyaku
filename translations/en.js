c1968daf3565813f35779874e86d15ff11aed3eb
AndreyMpr <andreympr@gmail.com>
2024-02-22 00:14:43 +0900

    Add english translation for the explanation text at the top of the page.

--- translations/en.js
+++ translations/en.js
@@ -1,5 +1,7 @@
 window.onload = function() {
     document.getElementById("langEn").addEventListener("click", function() {
         document.getElementById("formTitle").textContent = "Contract cancellation form";
+        document.getElementById("explanation-top").textContent = "If you wish to cancel your contract, please apply for cancellation through this form.\nFor items such as your new address that are not yet decided, please fill in “Undecided”.\nAfter we receive your cancellation, we will contact you at the phone number you provided.";
+        document.getElementById("explanation-bottom").textContent = "Shortest cancellation date: 2 months or 1 month (please check your contract).\nFor example: If applied on July 21st, the cancellation would be on September 21st (in case of 2 months advance notice) or August 21st (in case of 2 months advance notice).";
     }, 100);
 };
