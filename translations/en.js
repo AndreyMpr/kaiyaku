2a4c4dcc4f467471255638176bde8f393470d73f
AndreyMpr <andreympr@gmail.com>
2024-02-21 23:34:02 +0900

    Add a script that translates the title of the page to english.

---
+++ translations/en.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("langEn").addEventListener("click", function() {
+        document.getElementById("formTitle").textContent = "Contract cancellation form";
+    }, 100);
+};
