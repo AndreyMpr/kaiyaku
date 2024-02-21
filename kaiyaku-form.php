76f87dce82611bf5250543642d296dd42bde3cd7
AndreyMpr <andreympr@gmail.com>
2024-02-21 23:44:03 +0900

    Add unique id to different parts of the explanatory text tat the top of the page.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -18,13 +18,13 @@
 
     <h1 id="formTitle">解約受付フォーム</h1>
 
-    <div class="inner explanation">
+    <div class="inner explanation" id="explanation-top">
         <p>解約をご希望の際は、当フォームより解約申請をお願い致します。</p>
         <p>転居先のご住所等、未定の項目は「未定」とご記入ください。</p>
         <p>解約を受付後、ご入力いただいたお電話番号へ弊社より、ご連絡をさせていただきます。</p>
     </div>
 
-    <div class="inner explanation">
+    <div class="inner explanation" id="explanation-bottom">
         <p>最短解約日：2ヶ月　or　1ヶ月　（契約書をご確認ください）。</p>
         <p>例）7/21受付→9/21（2か月前）or　8/21（1ヶ月前）。</p>
     </div>
