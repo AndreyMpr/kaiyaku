e9954dc742b53a4b38403ae59d05398dd998841f
AndreyMpr <andreympr@gmail.com>
2024-02-23 23:03:18 +0900

    Add chinese translation file.

---
+++ translations/ch.php
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("langCh").addEventListener("click", function() {
+        document.getElementById("formTitle").textContent = "4y8437493839";
+    }, 100);
+};
--- translations/jp.js
+++ translations/jp.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     document.getElementById("langJp").addEventListener("click", function() {
         document.getElementById("formTitle").textContent = "解約受付フォーム";
-    });
+    }, 100);
 };
