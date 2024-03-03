a4ac0003286fa5332e5f99bd6899d30c9879e959
AndreyMpr <andreympr@gmail.com>
2024-03-04 07:51:17 +0900

    Fix property_names.php file path in the htmx hook, in the form.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -52,7 +52,7 @@
                 <div class="question">
                     <p class="number">2.</p>
                     <label class="wide-label" id="property-name-label" for="property-name">物件名</label>
-                    <input class="wide-input" type="text" id="property-name" name="propertyName" required placeholder="例：桜木マンション" hx-get="property_names.php" hx-trigger="keyup delay:500ms"">
+                    <input class="wide-input" type="text" id="property-name" name="propertyName" required placeholder="例：桜木マンション" hx-get="includes/property_names.php" hx-trigger="keyup delay:500ms"">
                     <p class="comment" id="property-name-comment">
                         ※ご契約書に記載されている建物名をご入力ください。
                     </p>
