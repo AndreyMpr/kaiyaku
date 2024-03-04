4d982df44877e7caf8c1793f9dd9ea87c1ee3888
AndreyMpr <andreympr@gmail.com>
2024-03-05 08:07:12 +0900

    Add several more property names to the autofill script.

--- includes/property_names.php
+++ includes/property_names.php
@@ -1,6 +1,6 @@
 <?php
 
-$propertyName = ["カレッジハウスⅠ","カレッジハウスⅡ","ルミエール"];
+$propertyName = ["カレッジハウスⅠ","カレッジハウスⅡ","カレッジハウスⅢ","カレッジハウスⅤ","カレッジハウス10","カレッジハウス11","ルミエール"];
 header('Content-Type: application/json');
 echo json_encode($propertyName);
 ?>
