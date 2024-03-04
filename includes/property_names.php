571f643c2574a045d853262775da0941454268c9
AndreyMpr <andreympr@gmail.com>
2024-03-05 08:08:21 +0900

    Swap roman numberals for arabic in property names script.

--- includes/property_names.php
+++ includes/property_names.php
@@ -1,6 +1,6 @@
 <?php
 
-$propertyName = ["カレッジハウスⅠ","カレッジハウスⅡ","カレッジハウスⅢ","カレッジハウスⅤ","カレッジハウス10","カレッジハウス11","ルミエール"];
+$propertyName = ["カレッジハウス1","カレッジハウス2","カレッジハウス3","カレッジハウス5","カレッジハウス10","カレッジハウス11","ルミエール"];
 header('Content-Type: application/json');
 echo json_encode($propertyName);
 ?>
