cd0b57c53ff511432bcb4531cbf0fd603b0ffae4
AndreyMpr <andreympr@gmail.com>
2024-03-15 07:09:06 +0900

    Edit dummy comments in vietnamese translation.

--- translations/vn.js
+++ translations/vn.js
@@ -37,15 +37,15 @@ window.addEventListener("load", function() {
         document.getElementById("fieldset-other").placeholder = "Vì chuyển công tác ra tỉnh khác";
         document.getElementById("address-label").textContent = "Địa chỉ di chuyển";
         document.getElementById("address").placeholder = "1-518-2 Oseicho, Omiya Ward, Saitama City, Saitama Prefecture";
-        document.getElementById("address-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("address-comment").textContent = "* Địa chỉ có thể được sử dụng để gửi các tài liệu hoặc chuyển khoản thanh toán hủy bỏ.";
         document.getElementById("bank-account-label").textContent = "Tài khoản hoàn trả tiền đặt cọc, v.v.";
         document.getElementById("bank-account").textContent = "Ví dụ: Ngân hàng Resona Saitama Omiya Nishi Chi nhánh Bình thường 1474654";
-        document.getElementById("bank-account-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("bank-account-comment").textContent = "* Tài khoản này sẽ được sử dụng để chuyển khoản thanh toán hủy bỏ hoặc tiền đặt cọc.";
         document.getElementById("bank-name-label").textContent = "Tên tài khoản";
-        document.getElementById("bank-name").textContent = "Ví dụ: Ka） Saitamamaruyamakomusho";
-        document.getElementById("bank-name-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("bank-name").textContent = "Ví dụ: カ）サイタママルヤマコウムショ";
+        document.getElementById("bank-name-comment").textContent = "* Vui lòng điền tên chủ tài khoản, như được ghi trong hợp đồng của bạn với ngân hàng (thông thường là katakana).";
         document.getElementById("comment-label").textContent = "Có vấn đề gì xảy ra trong thời gian bạn ở không?";
-        document.getElementById("comment-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("comment-comment").textContent = "* Nếu có bất kỳ vấn đề nào có thể ảnh hưởng đến quyết định chuyển đi của bạn, vui lòng cho chúng tôi biết về nó.";
         document.getElementById("satisfaction-legend").textContent = "Hãy cho chúng tôi biết mức độ hài lòng của bạn với công ty quản lý căn hộ Maruyama";
         document.querySelector("label[for='terrible']").textContent = "Không hài lòng";
         document.querySelector("label[for='bad']").textContent = "Hơi không hài lòng";
@@ -53,11 +53,11 @@ window.addEventListener("load", function() {
         document.querySelector("label[for='good']").textContent = "Hơi hài lòng";
         document.querySelector("label[for='great']").textContent = "Rất hài lòng";
         document.getElementById("explanation-label").textContent = "Nếu bạn đã trả lời 'Hơi không hài lòng' hoặc 'Không hài lòng' trong câu hỏi trên, hãy cho chúng tôi biết lý do.";
-        document.getElementById("explanation-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("explanation-comment").textContent = "Chúng tôi đánh giá cao phản hồi của bạn.";
         document.getElementById("wishes-label").textContent = "Có cơ sở hoặc dịch vụ nào bạn mong muốn không?";
-        document.getElementById("wishes-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("wishes-comment").textContent = "Chúng tôi đánh giá cao phản hồi của bạn.";
         document.getElementById("other-label").textContent = "Nếu bạn có bất kỳ câu hỏi nào khác, vui lòng tự do điền vào.";
-        document.getElementById("other-comment").textContent = "※ Vui lòng điền tên của tòa nhà trong hợp đồng.";
+        document.getElementById("other-comment").textContent = "Chúng tôi đánh giá cao phản hồi của bạn.";
         document.getElementsByClassName("submit-button")[0].textContent = "Đăng ký";
         document.getElementById("privacy-policy").textContent = "Chính sách bảo vệ thông tin cá nhân";
 
