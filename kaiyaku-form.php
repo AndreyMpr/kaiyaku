6aee3e573f2c81c396937274c89bbdf5b3d12edd
AndreyMpr <andreympr@gmail.com>
2024-03-19 22:13:46 +0900

    Add link to the privacy policy page on the koumusho website.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -384,7 +384,7 @@
         </section>
 
         <div id="privacy-policy">
-            <a>個人情報保護方針</a>
+            <a href="https://saitama-life.com/privacy/">個人情報保護方針</a>
         </div>
         
         <script src="translations/jp.js"></script>
