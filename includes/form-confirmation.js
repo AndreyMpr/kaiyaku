62c6a56cbaed915ed7c9d7bab9e5680724148d32
AndreyMpr <andreympr@gmail.com>
2024-03-24 11:55:48 +0900

    Typo in the confirmInput function declaration.

--- includes/form-confirmation.js
+++ includes/form-confirmation.js
@@ -1,7 +1,8 @@
 function confirmInput() {
+
     const contractorName = document.getElementById("contractor-name").value;
     const propertyName = document.getElementById("property-name").value;
-    const oomNumber = document.getElementById("room-number").value;
+    const roomNumber = document.getElementById("room-number").value;
     const parkingNumber = document.getElementById("parking-number").value;
     const phoneNumber = document.getElementById("phone-number").value;
     const moveOutDate = document.getElementById("move-out-date").value;
