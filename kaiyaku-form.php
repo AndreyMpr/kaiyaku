0b803fc9858de7a34f4b613364c9c7c819b4b7a6
AndreyMpr <andreympr@gmail.com>
2024-03-31 22:34:49 +0900

    Deleate link to htmx at the top of the html page.

--- kaiyaku-form.php
+++ kaiyaku-form.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>解約受付フォーム</title>
         <link rel="stylesheet" href="kaiyaku-form.css" />
-        <!--<script src="https://unpkg.com/htmx.org@1.9.10"></script>-->
+        
     </head>
 
     <body>
