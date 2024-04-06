c9eadf766a7ad000e5b78a56123689a4b3f67dbd
AndreyMpr <andreympr@gmail.com>
2024-04-06 23:37:50 +0900

    Create a connection to the database in the submit-form script.

--- includes/submit-form.php
+++ includes/submit-form.php
@@ -1,6 +1,8 @@
 <?php
 
-$kaiyakuDbHost = getenv('DB_PASS');
+$kaiyakuDbHost = getenv('DB_HOST');
 $kaiyakuDbName = getenv('DB_PASS');
 $kaiyakuDbUser = getenv('DB_USER');
 $kaiyakuDbPass = getenv('DB_PASS');
+
+$dbConnect = new mysqli($kaiyakuDbHost, $kaiyakuDbName, $kaiyakuDbUser, $kaiyakuDbPass);
