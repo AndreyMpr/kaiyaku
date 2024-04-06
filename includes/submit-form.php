ed104187496e6e4990096c31fb57919a6f9991b2
AndreyMpr <andreympr@gmail.com>
2024-04-06 23:48:08 +0900

    In the script that connects form to the database, add basic connection error handling.

--- includes/submit-form.php
+++ includes/submit-form.php
@@ -6,3 +6,7 @@ $kaiyakuDbUser = getenv('DB_USER');
 $kaiyakuDbPass = getenv('DB_PASS');
 
 $dbConnect = new mysqli($kaiyakuDbHost, $kaiyakuDbName, $kaiyakuDbUser, $kaiyakuDbPass);
+
+if ($dbConnect-> connect_errno) {
+    die("Connection error ". $dbConnect->connect_error);
+}
