baf45344de67f87c57094657d092333f25b8f4dc
AndreyMpr <andreympr@gmail.com>
2024-04-06 23:55:53 +0900

    Add some comments to the script that connectis form to the database.

--- includes/submit-form.php
+++ includes/submit-form.php
@@ -5,7 +5,11 @@ $kaiyakuDbName = getenv('DB_PASS');
 $kaiyakuDbUser = getenv('DB_USER');
 $kaiyakuDbPass = getenv('DB_PASS');
 
+//Connect to the database
+
 $dbConnect = new mysqli($kaiyakuDbHost, $kaiyakuDbName, $kaiyakuDbUser, $kaiyakuDbPass);
+
+//If connection errors
 
 if ($dbConnect-> connect_errno) {
     die("Connection error ". $dbConnect->connect_error);
