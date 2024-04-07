9a29b0d5e5612569e4904aaef1dfde21b815f9e1
AndreyMpr <andreympr@gmail.com>
2024-04-08 07:01:58 +0900

    Fix input sanitization in the submit-form php.

--- includes/submit-form.php
+++ includes/submit-form.php
@@ -17,22 +17,22 @@ if ($dbConnect-> connect_errno) {
 
 // Input sanitization
 
-$contractorName = mysqli_real_escape_string(dbConnect, $_POST['contractorName']);
-$propertyName = mysqli_real_escape_string(dbConnect, $_POST['propertyName']);
-$roomNumber = mysqli_real_escape_string(dbConnect, $_POST['roomNumber']);
-$parkingNumber = mysqli_real_escape_string(dbConnect, $_POST['parkingNumber']);
-$phoneNumber = mysqli_real_escape_string(dbConnect, $_POST['phoneNumber']);
-$moveOutDate = mysqli_real_escape_string(dbConnect, $_POST['moveOutDate']);
-$moveOutTime = mysqli_real_escape_string(dbConnect, $_POST['moveOutTime']);
-$reason = mysqli_real_escape_string(dbConnect, $_POST['reason']);
-$address = mysqli_real_escape_string(dbConnect, $_POST['address']);
-$bankAccount = mysqli_real_escape_string(dbConnect, $_POST['bankAccount']);
-$bankAccountName = mysqli_real_escape_string(dbConnect, $_POST['bankAccountName']);
-$comment = mysqli_real_escape_string(dbConnect, $_POST['comment']);
-$satisfaction = mysqli_real_escape_string(dbConnect, $_POST['satisfaction']);
-$explanation = mysqli_real_escape_string(dbConnect, $_POST['explanation']);
-$wishes = mysqli_real_escape_string(dbConnect, $_POST['wishes']);
-$other = mysqli_real_escape_string(dbConnect, $_POST['other']);
+$contractorName = mysqli_real_escape_string($dbConnect, $_POST['contractorName']);
+$propertyName = mysqli_real_escape_string($dbConnect, $_POST['propertyName']);
+$roomNumber = mysqli_real_escape_string($dbConnect, $_POST['roomNumber']);
+$parkingNumber = mysqli_real_escape_string($dbConnect, $_POST['parkingNumber']);
+$phoneNumber = mysqli_real_escape_string($dbConnect, $_POST['phoneNumber']);
+$moveOutDate = mysqli_real_escape_string($dbConnect, $_POST['moveOutDate']);
+$moveOutTime = mysqli_real_escape_string($dbConnect, $_POST['moveOutTime']);
+$reason = mysqli_real_escape_string($dbConnect, $_POST['reason']);
+$address = mysqli_real_escape_string($dbConnect, $_POST['address']);
+$bankAccount = mysqli_real_escape_string($dbConnect, $_POST['bankAccount']);
+$bankAccountName = mysqli_real_escape_string($dbConnect, $_POST['bankAccountName']);
+$comment = mysqli_real_escape_string($dbConnect, $_POST['comment']);
+$satisfaction = mysqli_real_escape_string($dbConnect, $_POST['satisfaction']);
+$explanation = mysqli_real_escape_string($dbConnect, $_POST['explanation']);
+$wishes = mysqli_real_escape_string($dbConnect, $_POST['wishes']);
+$other = mysqli_real_escape_string($dbConnect, $_POST['other']);
 
 // SQL query
 
