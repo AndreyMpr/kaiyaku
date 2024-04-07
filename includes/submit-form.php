584785dec7d03026c80d90fd1d3b0e4bfbd91dbb
AndreyMpr <andreympr@gmail.com>
2024-04-07 22:25:58 +0900

    Add input sanitization to the submit-form script.

--- includes/submit-form.php
+++ includes/submit-form.php
@@ -14,3 +14,22 @@ $dbConnect = new mysqli($kaiyakuDbHost, $kaiyakuDbName, $kaiyakuDbUser, $kaiyaku
 if ($dbConnect-> connect_errno) {
     die("Connection error ". $dbConnect->connect_error);
 }
+
+// Input sanitization
+
+$contractorName = mysqli_real_escape_string(dbConnect, $_POST['contractorName']);
+$propertyName = mysqli_real_escape_string(dbConnect, $_POST['propertyName']);
+$roomNumber = mysqli_real_escape_string(dbConnect, $_POST['roomNumber']);
+$parkingNumber = mysqli_real_escape_string(dbConnect, $_POST['parkingNumber']);
+$phoneNumber = mysqli_real_escape_string(dbConnect, $_POST['phoneNumber']);
+$moveOutDate = mysqli_real_escape_string(dbConnect, $_POST['moveOutDate']);
+$moveOutTime = mysqli_real_escape_string(dbConnect, $_POST['moveOutTime']);
+$reason = mysqli_real_escape_string(dbConnect, $_POST['reason']);
+$address = mysqli_real_escape_string(dbConnect, $_POST['address']);
+$bankAccount = mysqli_real_escape_string(dbConnect, $_POST['bankAccount']);
+$bankAccountName = mysqli_real_escape_string(dbConnect, $_POST['bankAccountName']);
+$comment = mysqli_real_escape_string(dbConnect, $_POST['comment']);
+$satisfaction = mysqli_real_escape_string(dbConnect, $_POST['satisfaction']);
+$explanation = mysqli_real_escape_string(dbConnect, $_POST['explanation']);
+$wishes = mysqli_real_escape_string(dbConnect, $_POST['wishes']);
+$other = mysqli_real_escape_string(dbConnect, $_POST['other']);
