e69a1d028e11492dc7c5872627378b2cbe61da15
AndreyMpr <andreympr@gmail.com>
2024-08-26 19:41:00 +0900

    Add english translation to the subtitle.

--- translations/top-en.js
+++ translations/top-en.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
     document.getElementById("langEn").addEventListener("click", function() {
-
+        document.querySelector(".mid-title h2").textContent = "Procedures for cancellation and moving out";
 
     });
 });
