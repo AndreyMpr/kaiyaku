207e8e5b457cda344644074d5dd48d3a1ee05395
AndreyMpr <andreympr@gmail.com>
2024-08-26 19:52:02 +0900

    Translate blue thank you message to english with inner text, to preserve line breaks.

--- translations/top-en.js
+++ translations/top-en.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
     document.getElementById("langEn").addEventListener("click", function() {
         document.querySelector(".mid-title h2").textContent = "Procedures for cancellation and moving out";
+        document.querySelector(".mid-subtitle h3").innerText = "To customers who wish to cancel their contract and move out\n\nThank you very much for choosing to live in a building\nmanaged by Saitama Maruyama engineering."; 
 
     });
 });
