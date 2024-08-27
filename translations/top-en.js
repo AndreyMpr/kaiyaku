b030f276abf70f9a51bfdb24673a301dd9ed8175
AndreyMpr <andreympr@gmail.com>
2024-08-27 17:37:45 +0900

    Translate blue tex-badge to english.

--- translations/top-en.js
+++ translations/top-en.js
@@ -4,6 +4,6 @@ window.addEventListener("load", function() {
         document.querySelector(".mid-subtitle h3").innerText = "To customers who wish to cancel their contract and move out\n\nThank you very much for choosing to live in a building\nmanaged by Saitama Maruyama engineering.";
         document.querySelector("#flex-title-left").textContent = "Contract cancellation request"; 
         document.querySelector(".flex-text-left").innerText = "Once you have decided to move, you will need to apply for contract cancellation. You can apply online (via cancellation application form) or in writing (by mailing the cancellation notice) that you received when you first signed the rental contract.\nUnfortunately, phone requests can not be accepted as cancellation notice.";
-
+        document.querySelector(".text-badge p").textContent = "Cancellation and moving out procedures"; 
     });
 });
