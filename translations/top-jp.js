b126a51f041eab0a4e81f3e5d70e91ee2f131ac8
AndreyMpr <andreympr@gmail.com>
2024-08-28 16:31:47 +0900

    Add a line to the explanation text into japanese translation script.

--- translations/top-jp.js
+++ translations/top-jp.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function() {
         document.querySelector(".mid-title h2").textContent = "解約・退去のお手続き方法";
         document.querySelector(".mid-subtitle h3").innerText = "解約・退去をご希望されるお客様へ\n\n株式会社埼玉丸山工務所の管理建物に\nお住まいいただきまして、\n誠にありがとうございました。";
         document.querySelector("#flex-title-left").textContent = "解約・退去のお申込み"; 
-        document.querySelector(".flex-text-left").innerText = "お引越しが決まりましたら「解約・退去」のお申込みが必要となります。お申込み方法は 「インターネット」(当社ご入居者様解約申込みフォーム)からのお申込み、または初回の賃貸契約時にお渡ししている「書面（解約通知書郵送）」によるお申込みとなります。";
+        document.querySelector(".flex-text-left").innerText = "お引越しが決まりましたら「解約・退去」のお申込みが必要となります。お申込み方法は 「インターネット」(当社ご入居者様解約申込みフォーム)からのお申込み、または初回の賃貸契約時にお渡ししている「書面（解約通知書郵送）」によるお申込みとなります。\nお電話・メールによるお申込みは出来ません。";
         document.querySelector(".text-badge p").textContent = "解約・退去のお手続きの流れ"; 
         document.querySelector(".flow-text p").textContent = '解約・退去のお申込みは、「解約受付日※1」から「退去日」までが「解約予告期間※2」未満の場合は、\n「解約受付日※1」より「解約予告期間※2」後までの家賃をご負担いただくことになりますので、日にちに余裕をもってのお手続きをお願いいたします。尚、当該手続きは賃貸借契約書に準ずるものといたします。';
         document.querySelector(".flow-asterisk-list li").textContent = '※1：「解約受付日」…インターネットでお申込みされた場合は申込み受付完了した日、書面でお申込みされた場合は\n解約通知書（ハガキ）の消印日となります。';
